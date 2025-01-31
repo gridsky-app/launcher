5420535a4f7aa5771a1167dfa213fad12af7c8e3
dxlliv <hello@dxlliv.dev>
2025-01-31 11:30:02 +0100

    Update docs folder

---
+++ docs/_nuxt/BH3oJLe4.js
@@ -0,0 +1,1 @@
+import{K as t}from"./BBTiQjmp.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
--- docs/_nuxt/DlWJo6My.js
+++
@@ -1,1 +0,0 @@
-import{K as t}from"./Dlz76QjY.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
